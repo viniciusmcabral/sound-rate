c254d547ffa6c72b9ac7d9ed8189c447a256901c
ViniciusMCabral <vmachakcabral@gmail.com>
2025-09-03 01:34:54 -0300

    Initial backend structure

---
+++ backend/src/main/java/com/viniciusmcabral/sound_rate/SoundRateApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusmcabral.sound_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundRateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/viniciusmcabral/sound_rate/SoundRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusmcabral.sound_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
