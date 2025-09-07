5526c8ecbafb8133daeaa4b36a3151e4391ec3a9
ViniciusMCabral <vmachakcabral@gmail.com>
2025-09-07 18:56:47 -0300

    Add async critical review scraper

--- backend/src/main/java/com/viniciusmcabral/sound_rate/SoundRateApplication.java
+++ backend/src/main/java/com/viniciusmcabral/sound_rate/SoundRateApplication.java
@@ -2,8 +2,10 @@ package com.viniciusmcabral.sound_rate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SoundRateApplication {
 
 	public static void main(String[] args) {
