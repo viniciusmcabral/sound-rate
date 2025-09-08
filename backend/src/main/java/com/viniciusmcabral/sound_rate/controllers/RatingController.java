e65bde7f8de45ac5588024feb66086c32fdca678
ViniciusMCabral <vmachakcabral@gmail.com>
2025-09-08 01:04:25 -0300

    Add Swagger OpenAPI documentation

--- backend/src/main/java/com/viniciusmcabral/sound_rate/controllers/RatingController.java
+++ backend/src/main/java/com/viniciusmcabral/sound_rate/controllers/RatingController.java
@@ -1,15 +1,25 @@
 package com.viniciusmcabral.sound_rate.controllers;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.viniciusmcabral.sound_rate.dtos.request.RatingRequestDTO;
 import com.viniciusmcabral.sound_rate.services.RatingService;
-import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/ratings")
+@SecurityRequirement(name = "bearerAuth")
 public class RatingController {
 
 	private final RatingService ratingService;
--- backend/src/main/java/com/viniciusmcabral/sound_rate/controllers/ReviewController.java
+++ backend/src/main/java/com/viniciusmcabral/sound_rate/controllers/ReviewController.java
@@ -1,15 +1,25 @@
 package com.viniciusmcabral.sound_rate.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.viniciusmcabral.sound_rate.dtos.request.ReviewRequestDTO;
 import com.viniciusmcabral.sound_rate.dtos.response.AlbumReviewDTO;
 import com.viniciusmcabral.sound_rate.services.ReviewService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reviews")
+@SecurityRequirement(name = "bearerAuth")
 public class ReviewController {
 
 	private final ReviewService reviewService;
